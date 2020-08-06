7fad194708ff341edb6d1f3231f67050d8470345
Grace Lindelien <gracelindelien@gmail.com>
2020-08-05 18:02:27 -0700

    Implement Schema for MySQL

--- database/schema.sql
+++ database/schema.sql
@@ -6,34 +6,45 @@ USE airbeebee;
 
 CREATE TABLE listings (
   ID INT AUTO_INCREMENT,
+  title VARCHAR(255) NOT NULL,
+  host VARCHAR(50) NOT NULL,
+  description TEXT NOT NULL,
+  guests INT NOT NULL,
+  rooms INT NOT NULL,
+  beds INT NOT NULL,
+  baths INT NOT NULL,
   PRIMARY KEY (ID)
 );
 
--- ADD TO LISTINGS
--- name
--- host
--- description
--- # of guests
--- # of rooms
--- # of beds
--- # of baths
+CREATE TABLE amenities (
+  ID INT AUTO_INCREMENT,
+  type VARCHAR(100),
+  icon VARCHAR(255),
+  PRIMARY KEY (ID)
+);
 
--- CREATE AMENITIES
--- ID (primary)
--- type
--- icon (URL)
+CREATE TABLE listings_amenities (
+  ID INT AUTO_INCREMENT,
+  listing_ID INT NOT NULL,
+  amenity_ID INT NOT NULL,
+  PRIMARY KEY (ID),
+  FOREIGN KEY (listing_ID) REFERENCES listings (id),
+  FOREIGN KEY (amenity_ID) REFERENCES amenities (id)
+);
 
--- CREATE LISTINGS_AMENITIES
--- ID (primary)
--- Listing_ID (FK)
--- Amenity_ID (FK)
+CREATE TABLE highlights (
+  ID INT AUTO_INCREMENT,
+  type VARCHAR(100),
+  description VARCHAR(255),
+  icon VARCHAR(255),
+  PRIMARY KEY (ID)
+);
 
--- CREATE HIGHLIGHTS
--- ID (primary)
--- amenity
--- icon (URL)
-
--- CREATE LISTINGS_HIGHLIGHTS
--- ID (primary)
--- Listing_ID
--- Highlight_ID
+CREATE TABLE listings_highlights (
+  ID INT AUTO_INCREMENT,
+  listing_ID INT NOT NULL,
+  highlight_ID INT NOT NULL,
+  PRIMARY KEY (ID),
+  FOREIGN KEY (listing_ID) REFERENCES listings (id),
+  FOREIGN KEY (highlight_ID) REFERENCES highlights (id)
+);
