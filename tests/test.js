04f96a8f83666ff0d281c10e14c51d51c2687134
Grace Lindelien <gracelindelien@gmail.com>
2020-08-11 12:06:03 -0700

    Setup test coverage reports w/ Jest

--- tests/test.js
+++ tests/test.js
@@ -1,9 +1,25 @@
+const axios = require('axios');
 // const generateCount = require('../database/dataSeed.js');
 
 const func = () => {
   return 1;
 };
 
-test('Jest should correctly test a basic function', () => {
-  expect (func()).toBe(1);
+describe('Jest Setup should be working', () => {
+  test('Jest should correctly test a basic function', () => {
+    expect(func()).toBe(1);
+  });
 });
+
+// describe('API calls to server should receive the correct response', () => {
+//   test('Should fetch correct info for Listing 1', () => {
+
+//   });
+// });
+
+// describe('Generate Count should return a random number within range', () => {
+//   test('Should return a number between 1 and 5, inclusive', () => {
+//     let value = generateCount(5, 1);
+//     expect(value).toBeGreaterThan(0);
+//   })
+// });
