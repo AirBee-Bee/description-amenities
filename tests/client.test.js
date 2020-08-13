7ccd6b25af5a6389920b26b5cc274e928b25fc4b
Grace Lindelien <gracelindelien@gmail.com>
2020-08-12 20:55:02 -0700

    Add tests for Amenities component

--- tests/client.test.js
+++ tests/client.test.js
@@ -14,7 +14,7 @@ Enzyme.configure({ adapter: new Adapter() });
 describe('Title Component', () => {
 
   test('div should exist with the correct text and image', () => {
-    const info = {
+    const props = {
       title: 'title',
       host: 'host',
       guests: 2,
@@ -23,7 +23,7 @@ describe('Title Component', () => {
       baths: 1,
       hostPhoto: 'URL'
     };
-    const title = mount(<Title info={info} />);
+    const title = mount(<Title info={props} />);
     expect(title).toExist();
     expect(title.find('img')).toExist();
     expect(title).toIncludeText('title');
@@ -36,10 +36,10 @@ describe('Title Component', () => {
 
 describe('Highlights Component', () => {
 
-  test('div should exist with the correct text and images', () => {
-    const highlights = [{ID: 1, url: 'URL', name: 'name'}];
-    const highlightsComponent = mount(<Highlights highlights={highlights} />);
-    expect(highlightsComponent).toExist();
+  test('div should exist with the correct text and image', () => {
+    const props = [{ID: 1, url: 'URL', name: 'name'}];
+    const highlightsComponent = mount(<Highlights highlights={props} />);
+    expect(highlightsComponent.find('div')).toExist();
     expect(highlightsComponent.find('img').prop('src')).toEqual('URL');
     expect(highlightsComponent).toIncludeText('name');
   });
@@ -53,6 +53,17 @@ describe('Description Component', () => {
     expect(description.find('div')).toExist();
     expect(description).toIncludeText('Hello');
     expect(description).toIncludeText('Contact host');
+  });
+
+});
+
+describe('Amenities Component', () => {
+
+  test('div should exist with the correct text and image', () => {
+    const props = [{name: 'amenity'}];
+    const amenities = mount(<Amenities amenities={props} />);
+    expect(amenities.find('div')).toExist();
+    expect(amenities).toIncludeText('amenity');
   });
 
 });
