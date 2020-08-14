8c3575aa75fb2088f924e9d49276361675f940e7
Grace Lindelien <gracelindelien@gmail.com>
2020-08-13 19:58:02 -0700

    Add icon images, adjust style and CSS

--- client/components/Amenities.jsx
+++ client/components/Amenities.jsx
@@ -4,16 +4,47 @@ import styled from 'styled-components';
 const StyledTitle = styled.div`
   font-size: 22px;
   font-weight: 500;
-  padding-bottom: 24px;
+  padding: 30px 0px 24px 0px;
 `
 
-function Amenities( {amenities} ) {
+const StyledContainer = styled.div`
+  display: flex;
+  align-items: stretch;
+  justify-content: flex-start;
+  flex-wrap: wrap;
+`
+
+const StyledAmenity = styled.div`
+  display: flex;
+  align-items: center;
+  width: 50%;
+  padding: 0px 0px 16px 0px;
+`
+
+const StyledIcon = styled.img`
+  width: 30px;
+  height: 30px;
+  padding: 0px 10px 0px 0px;
+`
+
+function Amenities({ amenities }) {
+  let topTen = amenities.slice(0, 10);
+
   return (
     <div>
       <StyledTitle>Amenities</StyledTitle>
-      {amenities.map(amn => {
-        return (<div key={amn.ID}>{amn.name}&nbsp;</div>)
-      })}
+      <StyledContainer>
+        {topTen.map(amn => {
+          return (
+            <StyledAmenity>
+              <div>
+                <StyledIcon src={amn.url} />
+              </div>
+              <div key={amn.ID}>{amn.name}&nbsp;</div>
+            </StyledAmenity>
+          )
+        })}
+      </StyledContainer>
     </div>
   );
 }
